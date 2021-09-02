42ac8610784f569a76d1e902590d03ca3bf08a9b
Trung <dh51704388@student.stu.edu.vn>
2021-09-02 13:05:13 +0700

    LOGIN TO DASHBOARD

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/layout/Landing";
+import Dashboard from "./views/Dashboard";
 import Auth from "./views/Auth";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Router>
         <Switch>
           <Route exact path="/" component={Landing} />
@@ -19,9 +21,10 @@ function App() {
             path="/login"
             render={(props) => <Auth {...props} authRoute="login" />}
           />
+          <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </Router>
-    </>
+    </AuthContextProvider>
   );
 }
 
--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,17 +1,52 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const LoginForm = () => {
+  //Context
+  const { loginUser } = useContext(AuthContext);
+
+  const [loginForm, setLoginForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  //Router
+  const history = useHistory();
+
+  //Local State
+  const { username, password } = loginForm;
+
+  const onChangeLoginForm = (event) =>
+    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
+
+  const login = async (event) => {
+    event.preventDefault();
+
+    try {
+      const loginData = await loginUser(loginForm);
+
+      if (loginData.success) {
+        history.push("/dashboard");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Form className="my-4">
+      <Form className="my-4" onSubmit={login}>
         <Form.Group>
           <Form.Control
             type="text"
             placeholder="Username..."
             name="username"
             required
+            value={username}
+            onChange={onChangeLoginForm}
           />
         </Form.Group>
         <Form.Group>
@@ -20,9 +55,11 @@ const LoginForm = () => {
             placeholder="Password..."
             name="password"
             required
+            value={password}
+            onChange={onChangeLoginForm}
           />
         </Form.Group>
-        <Button variant="success" className="mt-3">
+        <Button variant="success" className="mt-3" type="submit">
           Login
         </Button>
       </Form>
