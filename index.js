01f9d726909971e485d505ff4e6e8d239e4d8383
Trung <dh51704388@student.stu.edu.vn>
2021-09-04 13:21:40 +0700

    fixed heruku (4)

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-express.Router().use(function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
