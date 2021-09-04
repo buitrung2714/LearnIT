b8234fad6f6f20971911979005add8a7a1ea68a3
Trung <dh51704388@student.stu.edu.vn>
2021-09-04 15:42:42 +0700

    change

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://safe-dusk-87972.herokuapp.com/api"
+    ? "http://localhost:5000/api"
     : "https://rocky-lowlands-60986.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
