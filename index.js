23c90334905e67e1f5a9937f74a223ca7658d70e
Trung <dh51704388@student.stu.edu.vn>
2021-09-04 13:17:36 +0700

    fixed heruku (2)

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const cors = require("cors");
 
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
+var http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, {
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+    });
+    response.end("Hello World\n");
+  })
+  .listen(3000);
 
 const connectDB = async () => {
   try {
@@ -23,14 +35,8 @@ connectDB();
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 
