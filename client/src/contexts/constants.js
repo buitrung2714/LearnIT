97f133ff592a1c99a574344715b0aceb6f182470
Trung <dh51704388@student.stu.edu.vn>
2021-09-04 13:12:11 +0700

    deloy heruku

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
-    : "https://guarded-hamlet-56271.herokuapp.com/api";
+    ? "https://cryptic-beyond-17141.herokuapp.com/api"
+    : "https://cryptic-beyond-17141.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 
--- index.js
+++ index.js
@@ -23,6 +23,12 @@ connectDB();
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json());
 app.use(cors());
 
